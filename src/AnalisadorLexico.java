63ffdda8e4a702e03e3a95c3ad669f80a6c16305
Lucas Bitencourt <lucasbitencourt582@gmail.com>
2026-04-25 21:34:31 -0300

    feat: implementação do analisador léxico com AFD genérico

--- src/AnalisadorLexico.java
+++ src/AnalisadorLexico.java
@@ -2,12 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AnalisadorLexico {
-    
     private List<Token> tokens;
     private StringBuilder tokenAtual;
-    private int linha;
-    private int coluna;
-    private int colunaInicio;
+    private int linha, coluna, colunaInicio;
 
     private static final int SIMBOLO_L_VALIDO = 1;
     private static final int OPERADOR_ARITMETICO = 2;
@@ -21,25 +18,12 @@ public class AnalisadorLexico {
     public List<Token> analisar(String entrada) {
         tokens.clear();
         tokenAtual.setLength(0);
-        linha = 1;
-        coluna = 1;
-        colunaInicio = 1;
+        linha = 1; coluna = 1; colunaInicio = 1;
 
         for (int i = 0; i < entrada.length(); i++) {
             char c = entrada.charAt(i);
-            
-            if (c == '\n') {
-                finalizarTokenSeNecessario();
-                linha++;
-                coluna = 1;
-                continue;
-            }
-
-            if (isDelimitador(c)) {
-                finalizarTokenSeNecessario();
-                coluna++;
-                continue;
-            }
+            if (c == '\n') { finalizarTokenSeNecessario(); linha++; coluna = 1; continue; }
+            if (isDelimitador(c)) { finalizarTokenSeNecessario(); coluna++; continue; }
 
             switch (classificarCaractere(c)) {
                 case OPERADOR_ARITMETICO:
@@ -47,13 +31,11 @@ public class AnalisadorLexico {
                     tokens.add(new Token("operador aritmético", String.valueOf(c), linha, coluna));
                     coluna++;
                     break;
-
                 case SIMBOLO_L_VALIDO:
                     if (tokenAtual.length() == 0) colunaInicio = coluna;
                     tokenAtual.append(c);
                     coluna++;
                     break;
-
                 case SIMBOLO_INVALIDO:
                     finalizarTokenSeNecessario();
                     tokens.add(new Token("ERRO - símbolo inválido", String.valueOf(c), linha, coluna));
@@ -61,7 +43,6 @@ public class AnalisadorLexico {
                     break;
             }
         }
-
         finalizarTokenSeNecessario();
         return tokens;
     }
@@ -69,31 +50,19 @@ public class AnalisadorLexico {
     private void finalizarTokenSeNecessario() {
         if (tokenAtual.length() > 0) {
             String valor = tokenAtual.toString();
-            
             if (isValidoAFD(valor)) {
                 tokens.add(new Token("sentença válida", valor, linha, colunaInicio));
             } else {
                 tokens.add(new Token("ERRO - sentença inválida", valor, linha, colunaInicio));
             }
-            
             tokenAtual.setLength(0);
         }
     }
 
     private int classificarCaractere(char c) {
-        if (c >= 'a' && c <= 'e') {
-            return SIMBOLO_L_VALIDO;
-        }
-        
-        if (isOperadorAritmetico(c)) {
-            return OPERADOR_ARITMETICO;
-        }
-        
+        if (c >= 'a' && c <= 'e') return SIMBOLO_L_VALIDO;
+        if (c == '+' || c == '-' || c == '*' || c == '/') return OPERADOR_ARITMETICO;
         return SIMBOLO_INVALIDO;
-    }
-
-    private boolean isOperadorAritmetico(char c) {
-        return c == '+' || c == '-' || c == '*' || c == '/';
     }
 
     private boolean isDelimitador(char c) {
@@ -102,58 +71,50 @@ public class AnalisadorLexico {
 
     private boolean isValidoAFD(String palavra) {
         int estado = 0;
-        int m = 0;
-        int p = 0;
-        int nTotal = 0;
 
         for (int i = 0; i < palavra.length(); i++) {
             char c = palavra.charAt(i);
-
             switch (estado) {
                 case 0:
                     if (c == 'a') estado = 1;
-                    else if (c == 'c') { estado = 3; m++; }
-                    else if (c == 'd') estado = 4;
+                    else if (c == 'c') estado = 5;
+                    else if (c == 'd') estado = 6;
                     else return false;
                     break;
-
                 case 1:
-                    if (c == 'b') { estado = 2; nTotal++; }
+                    if (c == 'b') estado = 2;
                     else return false;
                     break;
-
                 case 2:
-                    if (c == 'a') estado = 1;
-                    else if (c == 'c') { estado = 3; m++; }
-                    else if (c == 'd') estado = 4;
+                    if (c == 'a') estado = 3;
                     else return false;
                     break;
-
                 case 3:
-                    if (c == 'c') m++;
-                    else if (c == 'd') estado = 4;
+                    if (c == 'b') estado = 0;
                     else return false;
                     break;
-
-                case 4:
-                    if (c == 'e') { estado = 5; p++; }
-                    else return false;
-                    break;
-
                 case 5:
-                    if (c == 'd') estado = 4;
+                    if (c == 'c') estado = 9;
+                    else if (c == 'd') estado = 7;
                     else return false;
                     break;
-
+                case 6:
+                    if (c == 'e') estado = 5;
+                    else return false;
+                    break;
+                case 7:
+                    if (c == 'e') estado = 9;
+                    else return false;
+                    break;
+                case 9:
+                    if (c == 'c') estado = 5;
+                    else if (c == 'd') estado = 6;
+                    else return false;
+                    break;
                 default:
                     return false;
             }
         }
-
-        boolean nPar = (nTotal % 2 == 0);
-        boolean mpImpar = ((m + p) % 2 != 0);
-        boolean estadoFinalValido = (estado == 0 || estado == 2 || estado == 3 || estado == 5);
-
-        return nPar && mpImpar && estadoFinalValido;
+        return (estado == 5);
     }
 }
